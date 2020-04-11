8f7de4bcd1774b90448f3dcd4468e189192532d3
Harsh rai <raiharsh88@gmail.com>
2020-04-11 12:19:02 +0530

    LCM

--- practice.js
+++ practice.js
@@ -1,50 +1,33 @@
-function parseBankAccount(text) {
-  text = text.split("\n");
-  text = text.map((line) => {
-    return line.split("");
-  });
-  let str = "";
-  let acNo = "";
-  text[0].forEach((ltr, idx) => {
-    str = "";
-    if (
-      ltr === " " &&
-      text[0][idx + 1] != "_" &&
-      text[1][idx - 1] != "_" &&
-      text[0][idx - 1] != "_"
-    ) {
-      str += text[1][idx] + text[2][idx];
-    }
-    if (str == "||") acNo += "1";
-    else {
-      str = "";
-      if (ltr == "_") str += "a";
-      if (text[1][idx + 1] == "|") str += "b";
-      if (text[2][idx + 1] == "|") str += "c";
-      if (text[2][idx] == "_") str += "d";
-      if (text[2][idx - 1] == "|") str += "e";
-      if (text[1][idx - 1] == "|") str += "f";
-      if (text[1][idx] == "_") str += "g";
-    }
+function sumFracts(l) {
+  function gcd(a, b) {
+    const max = Math.max(a, b);
+    const min = Math.min(a, b);
+    console.log(max, min);
 
-    if (str === "abdeg") acNo += "2";
-    else if (str === "abcdg") acNo += "3";
-    else if (str === "bcfg") acNo += "4";
-    else if (str === "acdfg") acNo += "5";
-    else if (str === "acdefg") acNo += "6";
-    else if (str === "abc") acNo += "7";
-    else if (str === "abcdefg") acNo += "8";
-    else if (str === "abcdfg") acNo += "9";
-    else if (str === "abcdef") acNo += "0";
-  });
+    return max % min === 0 ? min : gcd(max % min, min);
+  }
 
-  return parseInt(acNo);
+  function add([numer1, denom1], [numer2, denom2]) {
+    const numer = numer1 * denom2 + numer2 * denom1;
+    const denom = denom1 * denom2;
+    const g = gcd(numer, denom);
+
+    return [numer / g, denom / g];
+  }
+
+  if (l.length === 0) return null;
+
+  const [numer, denom] = l.reduce(([numer1, denom1], [numer2, denom2]) =>
+    add([numer1, denom1], [numer2, denom2])
+  );
+
+  return numer % denom === 0 ? numer / denom : [numer, denom];
 }
 
 console.log(
-  parseBankAccount(
-    "    _  _     _  _  _  _  _ \n" +
-      "  | _| _||_||_ |_   ||_||_|\n" +
-      "  ||_  _|  | _||_|  ||_| _|\n"
-  )
+  sumFracts([
+    [1, 2],
+    [1, 3],
+    [1, 4],
+  ])
 );
