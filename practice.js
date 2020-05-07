823fcbca06eac4226b958a70077725a762b25ff9
Harsh rai <raiharsh88@gmail.com>
2020-05-07 20:02:17 +0530

    next big  number

--- practice.js
+++ practice.js
@@ -1,70 +1,34 @@
-function sudoku(board) {
-  var set = board[0].map((e, i) => (i += 1));
-  var fact = Math.pow(set.length, 1 / 2);
+function nextBigNumber(number) {
+  number = number.toString().split("");
 
-  console.log(fact);
+  number = number.map((n) => parseInt(n));
 
-  // Validationg each column;
+  let collection = [];
 
-  for (let col of board) {
-    let temp = [...set];
+  for (let k = 0; k < number.length; k++) {
+    let current = number[k];
+    let cpy = [...number];
 
-    for (let element of col) {
-      if (temp.includes(element)) {
-        temp.splice(temp.indexOf(element), 1);
-      } else {
-        return false;
-      }
+    cpy.splice(k, 1);
+    for (let i = 0; i < number.length; i++) {
+      let temp = [...cpy];
+
+      temp.splice(i, 0, current);
+
+      temp = parseInt(temp.join(""));
+      //console.log(current, temp, cpy);
+
+      if (temp > parseInt(number.join(""))) collection.push(temp);
     }
   }
 
-  //Validating each row
+  return collection.find((n) => n === 59884848483559);
 
-  for (let i = 0; i < board.length; i++) {
-    let temp = [...set];
-
-    for (let j = 0; j < board.length; j++) {
-      if (temp.includes(board[j][i])) {
-        temp.splice(temp.indexOf(board[j][i]), 1);
-      } else {
-        return false;
-      }
-    }
-  }
-
-  let x = 0;
-  for (let m = 0; m < board.length; m += fact) {
-    for (let i = 0; i < board.length; i += fact) {
-      let temp = [...set];
-
-      for (let k = m; k < m + fact; k++) {
-        for (let l = i; l < i + fact; l++) {
-          if (temp.includes(board[k][l])) {
-            temp.splice(temp.indexOf(board[k][l]), 1);
-          } else {
-            return false;
-          }
-        }
-      }
-    }
-  }
-
-  console.log("x is ", x);
-  return true;
+  return collection;
+  if (collection.length < 1) return null;
+  return Math.min(...collection);
 }
 
-console.log(
-  sudoku([
-    [7, 8, 4, 1, 5, 9, 3, 2, 6],
-    [5, 3, 9, 6, 7, 2, 8, 4, 1],
-    [6, 1, 2, 4, 3, 8, 7, 5, 9],
+console.log(nextBigNumber(59884848459853));
 
-    [9, 2, 8, 7, 1, 5, 4, 6, 3],
-    [3, 5, 7, 8, 4, 6, 1, 9, 2],
-    [4, 6, 1, 9, 2, 3, 5, 8, 7],
-
-    [8, 7, 6, 3, 9, 4, 2, 1, 5],
-    [2, 4, 3, 5, 6, 1, 9, 7, 8],
-    [1, 9, 5, 2, 8, 7, 6, 3, 4],
-  ])
-);
+//        2017 => 2071
