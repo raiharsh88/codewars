c966d7afbf9f4b0fd3765126b984a5be7ed7767d
Harsh rai <raiharsh88@gmail.com>
2020-10-03 20:30:50 +0530

    basic features

--- dataCapture.js
+++ dataCapture.js
@@ -1,14 +1,12 @@
 const axios = require("axios");
 
 const fs = require("fs");
-const url =
-  "https://bff.oyorooms.com/v1/menu_items?client_type=Android%20App&additional_fields=ab_service_data,anonymous_user&user_mode=Consumer_Guest&version=20351&partner_app_version=20351&android_id=8879480a2e622e7e&idfa=&sid=1595103472897";
+const url = "";
 
 async function getData() {
   const res = await axios.get(url, {
     headers: {
-      access_token:
-        "QmpDZlRxZWo2UkZ5M3pSeHZ5NW46bi13NHN0ZTV5V1I1aGpCUVVHOUM6a3Z6OGlYMzhDb210eTV4YWduQTQ6ODA4MDA2MjgwMw==",
+      access_token: "",
     },
   });
 
